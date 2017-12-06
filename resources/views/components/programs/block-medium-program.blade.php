1c05e271dd6fe05b9e4bd42cd84d4e18b04be6b1
hungdomanh <hung150896@gmail.com>
2017-12-06 23:33:21 +0700

    fill medium block

--- resources/views/components/programs/block-medium-program.blade.php
+++ resources/views/components/programs/block-medium-program.blade.php
@@ -1,14 +1,22 @@
 <div class="blog_grid">
 	<h3><a href="blog_single.html">{{$program->name}} </a></h3><i class="document"> </i>
-	<a href="blog_single.html"><img src="{{asset('images/'.$program->image)}}" class="img-responsive" alt=""/></a>
+	<a href="blog_single.html"><img src="{{asset('images/programs/'.$program->image)}}" class="img-responsive" alt=""/></a>
 	<div class="singe_desc">
 		<p>{{$program->content}}</p>
 		<div class="comments">
 			<ul class="links">
 				<li><a href="#"><i class="blog_icon1"> </i><br><span>{{$program->created_at or 'Dec 3, 2017'}}</span></a></li>
-				<li><a href="#"><i class="blog_icon2"> </i><br><span>admin</span></a></li>
-				<li><a href="#"><i class="blog_icon3"> </i><br><span>1206</span></a></li>
-				<li><a href="#"><i class="blog_icon4"> </i><br><span>1206</span></a></li>
+				<li><a href="#"><i class="blog_icon2"> </i><br><span>{{$program->instructor}}</span></a></li>
+				<li><a href="#">
+				
+				@for ($i = 1; $i <= $program->level; $i++)
+					{{-- expr --}}
+					<i class="blog_icon3"> </i>
+				@endfor
+				
+				<br><span>Level {{$program->level}}</span></a></li>
+				{{--  --}}
+				{{-- <li><a href="#"><i class="blog_icon4"> </i><br><span>1206</span></a></li> --}}
 			</ul>
 			<div class="btn_blog"><a href="/programs/{{$program->id}}">Read More</a>
 			</div>
